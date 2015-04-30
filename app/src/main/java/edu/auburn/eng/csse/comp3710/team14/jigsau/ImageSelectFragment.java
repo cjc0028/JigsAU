92d805899af61805a5d81b7df9c65520ecdd1cf7
Chase Counsell <cjc0028@auburn.edu>
2015-04-30 16:10:35 -0500

    stuff

--- app/src/main/java/edu/auburn/eng/csse/comp3710/team14/jigsau/ImageSelectFragment.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team14/jigsau/ImageSelectFragment.java
@@ -6,20 +6,20 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class ImageSelectFragment extends Fragment implements View.OnClickListener {
 
     final private int PICK_IMAGE = 0;
-    ImageView image;
+    //ImageView image;
+    String imagePath;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,15 +42,8 @@ public class ImageSelectFragment extends Fragment implements View.OnClickListene
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
 
-                GameFragment gameFragment = new GameFragment();
-                FragmentTransaction transaction = getActivity().getFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_images, gameFragment);
-                transaction.addToBackStack(null);
-                image = (ImageView)getActivity().findViewById(R.id.image);
-
-                transaction.commit();
-
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
                 break;
         }
 
@@ -59,6 +52,7 @@ public class ImageSelectFragment extends Fragment implements View.OnClickListene
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         //getActivity().setContentView(R.layout.fragment_game);
+        //image = (ImageView)getActivity().findViewById(R.id.image);
 
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == PICK_IMAGE) {
@@ -69,14 +63,23 @@ public class ImageSelectFragment extends Fragment implements View.OnClickListene
 
                 try {
                     cursor.moveToFirst();
-                    String imagePath = cursor.getString(cursor.getColumnIndex(projection[0]));
-                    image.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+                    imagePath = cursor.getString(cursor.getColumnIndex(projection[0]));
                     cursor.close();
                 } catch (NullPointerException e) {
                     e.printStackTrace();
                 }
 
-
+                GameFragment gameFragment = new GameFragment();
+                Bundle bundle = new Bundle();
+                FragmentTransaction transaction = getActivity().getFragmentManager().beginTransaction();
+                bundle.putString("imagePath", imagePath);
+                gameFragment.setArguments(bundle);
+                transaction.replace(R.id.fragment_container, gameFragment);
+                Log.i("transaction", "Fragment replaced");
+                transaction.addToBackStack(null);
+                Log.i("transaction", "Added to Backstack");
+                transaction.commit();
+                Log.i("transaction", "Fragment committed");
             }
         }
     }
